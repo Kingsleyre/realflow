eab419e7ca8c19dbca321fb961f233b58100a1fb
Kingsley Robert-Ezenta <krobertezenta@gmail.com>
2023-03-14 17:25:51 -0600

    register

--- backend/src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express-promise-router')();
-const authController = require('../controllers/auth.controller');
-
-router.post('/login', authController.loginAuth);
-router.post('/logout', authController.logoutAuth);
-router.get('/active', authController.activeAuth);
-
-module.exports = router;
